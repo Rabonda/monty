11c08bbd5539137d9b25a012abc7eba1e0641ee5
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-16 16:24:24 +0200

    pall file

---
+++ m_pall.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * m_pall - A functiom that print the stack
+ * @h: double pointer
+ * @line_number: line count
+ * void function: Print the stack
+ */
+void m_pall(stack_t **h, unsigned int line_number)
+{
+	(void)(line_number);
+
+	if (*h && h)
+		for (stack_t *temporary = *head; temporary; temporary = temporary->next)
+			printf("%d\n", temporary->n);
+}
