db994b88e42354ccf5a606cde64557154f6b2267
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-16 16:33:54 +0200

    pop file

---
+++ m_pop.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * m_pop - A function that delete element top of stack.
+ * @h: double pointer to head.
+ * @line_number: line number
+ * Void function: returns delete element top of stack.
+ */
+void m_pop(stack_t **h, unsigned int line_number)
+{
+	stack_t *temporary;
+
+	if (*h == NULL || h == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", line_count);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	temporary = *h;
+	if (temporary->next)
+	{
+		temporary->next->prev = NULL;
+		*h = temporary->next;
+	}
+	else
+		*h = NULL;
+	free(temporary);
+}
