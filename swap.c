81492078b594d2c98fa15ad376a1a4d960bc849f
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-20 09:55:07 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,30 +1,30 @@
 #include "monty.h"
 
 /**
- * _swap - A function that swaps the top two elements of the stack.
+ * m_swap - A function that swaps the top two elements of the stack.
  * @stack: double pointer to the top (head) of the stack.
  * @line_number: counter for line.
  *
  * void function: swaps the top two elements of the stack.
  */
-void _swap(stack_t **stack, unsigned int line_number)
+void m_swap(stack_t **h, unsigned int line_number);
 {
-	stack_t *temp;
+	stack_t *temporary;
 
-	if (!stack || !*stack || !((*stack)->next))
+	if (h == NULL || *h == NULL || !((*h)->next))
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free_stack_t(*stack);
+		free_stack_t(*h);
 
 		exit(EXIT_FAILURE);
 	}
 
-	temp = *stack;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	if ((*stack)->next)
-		((*stack)->next)->prev = temp;
-	temp->next = (*stack)->next;
-	(*stack)->next = temp;
-	temp->prev = *stack;
+	temporary = *h;
+	*h = (*h)->next;
+	(*h)->prev = NULL;
+	if ((*h)->next)
+		((*h)->next)->prev = temporary;
+	temporary->next = (*h)->next;
+	(*h)->next = temporary;
+	temporary->prev = *h;
 }
